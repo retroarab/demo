b6f9338cee41b1aac555f54008815955f12a1d56
retroarab <iosifdala@yahoo.com>
2023-04-19 04:50:23 +0300

    Changes to pages and design

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -10,6 +10,8 @@ module com.example.demo {
             requires org.kordamp.bootstrapfx.core;
             requires com.almasb.fxgl.all;
     requires java.logging;
+    requires java.management;
+    requires jdk.management;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
